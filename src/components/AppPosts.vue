6a51d525722c7d18a146f52a8b926fa8a6b5df9e
hiki89 <markohitl89@gmail.com>
2018-08-16 20:22:21 +0200

    Zadatak 2

--- src/components/AppPosts.vue
+++ src/components/AppPosts.vue
@@ -1,12 +1,33 @@
 <template>
     <div>
-
+        <PostsList :posts="posts" />
     </div>
 </template>
 
 <script>
+import PostsList from '../components/PostsList'
+import { posts } from '../services/Posts'
+
 export default {
-    
+    components: {
+        PostsList
+    },
+
+    data() {
+        return {
+            posts: []
+        }
+    },
+
+    beforeRouteEnter(to, from, next) {
+        posts.getAll()
+            .then((response) => {
+                next((vm) => {
+                    vm.posts = response.data
+                })
+            })
+        
+    } 
 }
 </script>
 
---
+++ src/components/PostsList.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <ul class="list-group list-group-flush" v-for="(post, key) in posts" :key="key">
+            <li class="list-group-item">Title: {{ post.title }}</li>
+            <li class="list-group-item">Text: {{ post.text }}</li>
+            <li class="list-group-item">Created at: {{ post.createdAt }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['posts']
+}
+</script>
+
